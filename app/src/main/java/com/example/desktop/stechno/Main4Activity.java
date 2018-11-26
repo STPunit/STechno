435a03747ff35d90fe3317bc6bb3c9f888019cc1
DESKTOP <smarttechno.punit@gmail.com>
2018-11-26 11:38:18 +0530

    Mostly completed 1

--- app/src/main/java/com/example/desktop/stechno/Main4Activity.java
+++ app/src/main/java/com/example/desktop/stechno/Main4Activity.java
@@ -79,38 +79,6 @@ Comadp comadp;
         });
 
 
-//        ChildEventListener childEventListener = new ChildEventListener() {
-//            @Override
-//            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//                    Profile p = dataSnapshot.getValue(Profile.class);
-//                    list.add(p);
-//                comadp = new Comadp(Main4Activity.this, list);
-//                Taskcom.setAdapter(comadp);
-//
-//            }
-//
-//
-//            @Override
-//            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//
-//            }
-//
-//            @Override
-//            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-//
-//            }
-//
-//            @Override
-//            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        };
-//        query.addChildEventListener(childEventListener);
 
 
     }
--- app/src/main/java/com/example/desktop/stechno/Main6Activity.java
+++ app/src/main/java/com/example/desktop/stechno/Main6Activity.java
@@ -2,9 +2,11 @@ package com.example.desktop.stechno;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toolbar;
@@ -88,6 +90,7 @@ public class Main6Activity extends AppCompatActivity {
 
             }
         });
+
 
     }
 
