d9f9f3424d7668d185b3bf8103e0bc10655ae39f
DESKTOP <smarttechno.punit@gmail.com>
2018-11-30 16:51:36 +0530

    text clipping issue solved

--- app/src/main/java/com/example/desktop/stechno/MainActivity.java
+++ app/src/main/java/com/example/desktop/stechno/MainActivity.java
@@ -14,9 +14,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 
@@ -28,10 +35,12 @@ public class MainActivity  extends AppCompatActivity {
 
 
 
-    Button button2, t2, t3;
+    Button button2,  t3;
     ActionBarDrawerToggle actionBarDrawerToggle;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    TextView TaskCom, TaskPro, TaskPen;
+    FirebaseDatabase firebaseDatabase;
    //Toolbar toolbar;
     android.support.v7.widget.Toolbar toolbar;
 
@@ -43,50 +52,18 @@ public class MainActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tbr();
         button2 = findViewById(R.id.button2);
-        t2 = findViewById(R.id.t2);
         t3 = findViewById(R.id.t3);
-//        navigationView = findViewById(R.id.navview);
-//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//                Toast.makeText(MainActivity.this," skdjasl", Toast.LENGTH_SHORT).show();
-//                switch (menuItem.getItemId()){
-//
-//                    case R.id.addTask:
-//                        Toast.makeText(MainActivity.this, "add task", Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case R.id.viewTask:
-//                        Toast.makeText(MainActivity.this,"view", Toast.LENGTH_SHORT).show();
-//                        break;
-//                }
-//                return false;
-//            }
-//        });
-      //  toolbar = findViewById(R.id.tbr);
-      //  navigationView.setNavigationItemSelectedListener(this);
-//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected( MenuItem menuItem) {
-//                if (actionBarDrawerToggle.onOptionsItemSelected(menuItem))
-//                switch (menuItem.getItemId()){
-//                    case R.id.addTask:
-//                        startActivity(new Intent(MainActivity.this,Main2Activity.class));
-//                        menuItem.setChecked(true);
-//                        break;
-//                       // return true;
-//                    case R.id.viewTask:
-//                        startActivity(new Intent(MainActivity.this, Main7Activity.class));
-//                        menuItem.setChecked(true);
-//                        break;
-////                        default:
-//                           // return MainActivity.super.onOptionsItemSelected(menuItem);
-//                }
-//
-//            return true;
-//              //  return MainActivity.super.onOptionsItemSelected(menuItem);
-//            }
-//
-//        });
+        TaskPro = findViewById(R.id.TaskPro);
+        TaskPen = findViewById(R.id.TaskPen);
+        TaskCom = findViewById(R.id.TaskCom);
+
+        DatabaseReference tsk = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference tsk1 = tsk.child("New Task").getRef();
+
+
+        Query query = tsk1.orderByChild("taskStatus").equalTo("COMPLETED");
+        Query query1 = tsk1.orderByChild("taskStatus").equalTo("PENDING");
+        Query query2 = tsk1.orderByChild("taskStatus").equalTo("IN_PROGRESS");
 
 
         button2.setOnClickListener(new View.OnClickListener() {
@@ -102,14 +79,7 @@ public class MainActivity  extends AppCompatActivity {
 
             }
         });
-        t2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i1 = new Intent(MainActivity.this, Main3Activity.class);
-                startActivity(i1);
-                ;
-            }
-        });
+
         t3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,95 +88,79 @@ public class MainActivity  extends AppCompatActivity {
                 ;
             }
         });
+
+
+
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                long count = dataSnapshot.getChildrenCount();
+                String cm  = Long.toString(count);
+                TaskCom.setText(cm);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        query.addValueEventListener(eventListener);
+
+
+
+//        tsk1.orderByChild("COMPLETED").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                Long countpo = dataSnapshot.getChildrenCount();
+//                String stpo = Long.toString(countpo);
+//                TaskPro.setText(stpo);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+
+
+        ValueEventListener eventListener1 = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                long countpo = dataSnapshot.getChildrenCount();
+                String stpo = Long.toString(countpo);
+                TaskPro.setText(stpo);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        query2.addValueEventListener(eventListener1);
+
+
+        ValueEventListener eventListener2 = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                long countip = dataSnapshot.getChildrenCount();
+                String stip = Long.toString(countip);
+
+                TaskPen.setText(stip);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        query1.addValueEventListener(eventListener2);
+
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (actionBarDrawerToggle.onOptionsItemSelected(item))
-//            return true;
-//
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
-
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//        if (actionBarDrawerToggle.onOptionsItemSelected(menuItem)){
-//            switch (menuItem.getItemId()){
-//
-//                case R.id.addTask:
-//                        startActivity(new Intent(MainActivity.this,Main2Activity.class));
-//                        menuItem.setChecked(true);
-//                        break;
-//                       // return true;
-//                    case R.id.viewTask:
-//                        startActivity(new Intent(MainActivity.this, Main7Activity.class));
-//                        menuItem.setChecked(true);
-//                        break;
-////                        default:
-//
-//
-//            }
-//        }
-//        drawerLayout.closeDrawer(GravityCompat.START);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//
-//        getMenuInflater().inflate(R.menu.nav_menu, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        Toast.makeText(MainActivity.this,"clicked", Toast.LENGTH_SHORT).show();
-//        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
-//
-//            switch (item.getItemId()){
-//                case R.id.addTask:
-//                    startActivity(new Intent(MainActivity.this, Main2Activity.class));
-//                    Toast.makeText(MainActivity.this, item.getItemId(), Toast.LENGTH_SHORT).show();
-//                    return true;
-//                case R.id.viewTask:
-//                    startActivity(new Intent(MainActivity.this, Main2Activity.class));
-//                    Toast.makeText(MainActivity.this, item.getItemId(), Toast.LENGTH_SHORT).show();
-//                    return true;
-//                    default:
-//                        return super.onOptionsItemSelected(item);
-//
-//            }
-//        }
-//        return true;
-//
-//    }
-
-    //
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//        Toast.makeText(MainActivity.this,"clicked", Toast.LENGTH_LONG).show();
-//        if (actionBarDrawerToggle.onOptionsItemSelected(menuItem)){
-//            switch (menuItem.getItemId()){
-//                case R.id.addTask:
-//                    startActivity(new Intent(MainActivity.this, Main2Activity.class));
-//                case R.id.viewTask:
-//                    startActivity(new Intent(MainActivity.this, Main2Activity.class));
-//
-//            }
-//
-//        }
-//        return super.onOptionsItemSelected(menuItem);
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
     private void tbr(){
         toolbar  = findViewById(R.id.tbr);
         drawerLayout = findViewById(R.id.drawid);
